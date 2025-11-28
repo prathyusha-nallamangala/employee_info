a70c10ae1b7406ff252d4e06e6efc7806a4efc45
prathyusha-nallamangala <124361583+prathyusha-nallamangala@users.noreply.github.com>
2025-11-28 16:38:16 +0530

    Update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-const express = require('express');const dotenv = require('dotenv');const connectDB = require('./config/db');const cors = require('cors');const taskRoutes = require('./routes/taskRoutes');dotenv.config();connectDB();const app = express();app.use(cors());app.use(express.json());app.use('/api/tasks', taskRoutes);const PORT = process.env.PORT || 5000;app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
+
+// Connect to the database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Body parser
+app.use(cors()); // Enable CORS
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
